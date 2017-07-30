e7d643a5b1ffb0a96ca32a157873a47efb86bbba
Beka Tsotsoria <b.tsotsoria@gmail.com>
2017-07-30 22:25:21 +0400

    Simple message broker made single-threaded

--- server/src/main/java/com/example/rps/WebSocketConfig.java
+++ server/src/main/java/com/example/rps/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.rps;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -22,5 +23,21 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+
+    /*
+        Fix the issue of out of order messages.
+        However this significantly impacts scalability, in real-world scenario more sophisticated broker
+        should be used, for ex: ActiveMQ
+        https://stackoverflow.com/questions/29689838/sockjs-receive-stomp-messages-from-spring-websocket-out-of-order
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(1);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(1);
     }
 }
