93e725d959d087b971fa47af8a7931f73b25d6ac
Beka Tsotsoria <b.tsotsoria@gmail.com>
2017-07-31 01:59:43 +0400

    Merged web module back into server for simplified packaging

--- server/src/main/java/com/example/rps/Server.java
+++ server/src/main/java/com/example/rps/Server.java
@@ -4,12 +4,28 @@ package com.example.rps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Beka Tsotsoria
  */
+@Controller
 @SpringBootApplication
 public class Server {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/game/{name}")
+    public String game(@PathVariable String name, ModelMap model) {
+        model.addAttribute("game", name);
+        return "game";
+    }
 
     public static void main(String[] args) {
         start(args);
